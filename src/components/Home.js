47f0c792a98dcb883bed32f9a0b005f7c6ea111b
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-24 01:54:20 +0300

    navbar css

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/Home.css'
+import { Navbar } from './Navbar'
+import { Product } from './Product'
+export const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Product/>
+    </div>
+  )
+}
